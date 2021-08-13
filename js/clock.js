ea652ec470d186dc13fb789e9539bf549edfca11
국산자몽 <50566360+K-Grapefruit@users.noreply.github.com>
2021-08-13 12:12:59 +0900

    0813

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,18 @@
 const clock = document.querySelector("h2#clock");
+const day = document.getElementById("today");
 
 function getClock() {
   const date = new Date();
+
+  const thisyear = date.getFullYear();
+  const thisMonth = String(date.getMonth() + 1).padStart(2, "0");
+  const today = String(date.getDate()).padStart(2, "0");
+
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
   clock.innerText = `${hours}:${minutes}:${seconds}`;
+  day.innerText = `${thisyear} / ${thisMonth} / ${today}`;
 }
 
 getClock();
