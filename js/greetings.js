a7f93baab1015faa6a4bc1f2b880b44187cb2de7
국산자몽 <50566360+K-Grapefruit@users.noreply.github.com>
2021-09-05 19:14:22 +0900

    changeUI

--- js/greetings.js
+++ js/greetings.js
@@ -2,6 +2,9 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const todoform = document.querySelector("#todo-form");
+const todoframe = document.querySelector(".center_frame .todo_frame");
+const centerframe = document.querySelector(".center_frame");
+const center = "center_frame_width";
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -18,14 +21,23 @@ function onLoginSubmit(event) {
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  todoframe.classList.add("todo_frame");
+
+  centerframe.classList.remove(center);
+  centerframe.classList.add("center_frame");
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-console.log(todoform);
+console.log(todoframe);
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   todoform.classList.add(HIDDEN_CLASSNAME);
+  todoframe.classList.remove("todo_frame");
+  centerframe.classList.remove("center_frame");
+
+  centerframe.classList.add(center);
+
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
